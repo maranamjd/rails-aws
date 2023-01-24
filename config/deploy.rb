7ab593854f8c1f9750d208e1b111ba8a8475650a
maranamjd <mjd.marana@gmail.com>
2023-01-24 19:33:38 +0800

    Update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, "git@github.com:maranamjd/rails-aws.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/rails/rails-aws"
+set :deploy_to, "/home/aws-rails/rails-aws"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server "43.206.238.230", user: "ubuntu", roles: %w{web app db}
+server "43.206.238.230", user: "aws-rails", roles: %w{web app db}
 set :rails_env, 'production'
